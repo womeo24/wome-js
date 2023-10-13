bb44b7a418d3278900a98a1f4fbf8052e1853e7a
womeo24 <womeo24@gmail.com>
2023-10-13 13:32:35 +0100

    functions.js

--- script.js
+++ script.js
@@ -5,3 +5,48 @@ function myFunction() {
 function myFunctions() {
     window.alert('welcome ola')
 }
+
+
+
+
+// string
+let color = 'yellow';
+let person = 'gt';
+
+// numbers
+let lenght = 16;
+let weight = 12.5;
+
+// boolean
+let x = true;
+let y = false;
+
+// object
+const person = {firstName:"gt", lastName:"best"};
+
+// array
+const car = ['volvo', 'bmw', 'audi']
+
+// object
+const date = new Date('10-13-2023')
+
+
+// undefine
+let x;
+
+// boolean
+let x = 6;
+let y = 6;
+let z = 4;
+
+(x == y) // true
+(x == z) // false
+
+
+
+let x = myFunction(4, 3);
+document.getElementById('result').innerHTML = x;
+
+function myFunction(a, b) {
+    re
+}
